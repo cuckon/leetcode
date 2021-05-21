0c860b8e5aa62d6ace4bf4f5cec25da166a940ff
John <cuckons@gmail.com>
2021-05-21 21:20:30 +0800

    #94 initially works

---
+++ 94.binary-tree-inorder-traversal.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let r = [];
+
+    let f = function(node) {
+        if (!node) return
+        if (node.left)
+            f(node.left)
+        r.push(node.val)
+        if (node.right)
+            f(node.right)
+    };
+    f(root);
+    return r;
+};
+// @lc code=end
+
